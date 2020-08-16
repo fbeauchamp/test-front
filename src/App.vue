e2826810377be3beb5dec8b2492a070cf9e9b0b4
florent beauchamp <florent.beauchamp@gmail.com>
2020-08-16 11:17:07 +0200

    feat improve UI

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div id="app">
     <h1>Demo of calendar component</h1>
-    <select v-model="language" id="language">
-      <option value="fr">Français</option>
-      <option value="fr-ca">Français Canadien</option>
-      <option value="en">English</option>
-      <option value="zh-cn">中文</option>
+    <div style="display: flex; flex-direction: row;">
+      <div style="flex:1;">
+        <h2>Langue</h2>
+        <select v-model="language" id="language" style="
+  width: 19.8em;
+  height: 2em;
+  border-radius: 0.25em;
+  margin: 1em 0;">
+          <option value="fr">Français</option>
+          <option value="fr-ca">Français Canadien</option>
+          <option value="en">English</option>
+          <option value="zh-cn">中文</option>
 
-    </select>
-    <Calendar v-on:selectionChanged="selectionChanged" :language="language"/>
-    <ul>
-      <li v-for="date in selection" :key="date">
-        {{ date }}
-      </li>
-    </ul>
+        </select>
+        <h2>Date(s) sélectionnée(s)</h2>
+
+        <ul v-if="selection.length > 0">
+          <li v-for="date in selection" :key="date">
+            {{ date }}
+          </li>
+        </ul>
+        <span v-else>Aucune</span>
+      </div>
+      <Calendar v-on:selectionChanged="selectionChanged" :language="language"/>
+    </div>
+
   </div>
 </template>
 
